a361992ae051286ba39a578e89b7646a700d0040
hao <106147768@qq.com>
2022-06-29 11:41:31 +0800

    初始化

---
+++ entry/src/main/java/com/group1/buyit/MyApplication.java
@@ -0,0 +1,10 @@
+package com.group1.buyit;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/ohosTest/java/com/group1/buyit/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.group1.buyit;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.group1.buyit", actualBundleName);
+    }
+}
