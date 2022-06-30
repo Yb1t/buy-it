b5e5b49dc41b989467e0feae8e1318bac3be3775
hao <106147768@qq.com>
2022-06-30 13:28:06 +0800

    自定义导航栏组件

---
+++ entry/src/main/js/navbar/pages/index/index.js
@@ -0,0 +1,20 @@
+export default {
+    data: {
+        title: "",
+        defaultIndex:2,
+        menudata:[
+            {icon:"/common/icons/home.png", title:"主页", iconActive:"/common/icons/home_active.png"},
+            {icon:"/common/icons/boy.png", title:"发现", iconActive:"/common/icons/message_active.png"},
+            {icon:"/common/icons/message.png", title:"消息", iconActive:"/common/icons/message_active.png"},
+            {icon:"/common/icons/cart.png", title:"购物车", iconActive:"/common/icons/cart_active.png"},
+            {icon:"/common/icons/mine.png", title:"我的", iconActive:"/common/icons/mine_active.png"}
+        ],
+
+    },
+    changeMenu(idx){
+        this.$emit('onChangeMenu', {index: idx})
+        console.log(idx)
+        this.defaultIndex = idx
+    },
+
+}
