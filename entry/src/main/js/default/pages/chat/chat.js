5cf5714f55c5bd37686fd3d77f05a6e85c354f91
hao <106147768@qq.com>
2022-07-01 23:05:09 +0800

    1.实现WebSocket消息发送 2.逻辑优化 3.界面优化

--- entry/src/main/js/default/pages/chat/chat.js
+++ entry/src/main/js/default/pages/chat/chat.js
@@ -6,28 +6,61 @@ export default {
         title: 'World',
         topHeight: "100px",
         ws: null,
-        url: 'ws://', //ws://121.40.165.18:8800
-        sendTo: "user",
+        url: 'ws://haotest.vaiwan.com/websocket/1',
+        user: {
+            userId: 1,
+            username: "hao",
+            avatar: "/common/icons/user.png"
+        },
+        sendTo: {
+            userId: 2,
+            username: "other",
+            avatar: "/common/icons/user.png"
+        },
         isFriend: false,
         isOnline: false,
+        isSending: false,
         input: "",
         array: [
-            {avatar:"/common/icons/user.png", username: "user", msg:"你好",msgCount:"1", date:"22/06/29 11:00"},
-            {avatar:"/common/icons/user.png", username: "me", msg:"balbalba",msgCount:"10", date:"22/06/29 12:00"},
-            {avatar:"/common/icons/user.png", username: "user", msg:"你好你好你好",msgCount:"2", date:"22/06/29 00:00"},
-            {avatar:"/common/icons/user.png", username: "me", msg:"啦啦啦啦啦",msgCount:"0", date:"22/06/29 12:00"},
-            {avatar:"/common/icons/user.png", username: "user", msg:"哈哈哈哈哈",msgCount:"1", date:"22/06/29 20:00"}
+            {to: 1, sender: 2, msg:"你好", date: "22/06/29 12:00"},
+            {to: 2, sender: 1, msg:"balbalba", date:"22/06/29 12:00"},
+            {to: 1, sender: 2, msg:"你好你好你好", date:"22/06/29 00:00"},
+            {to: 2, sender: 1, msg:"啦啦啦啦啦", date:"22/06/29 12:00"},
+            {to: 1, sender: 2, msg:"哈哈哈哈哈", date:"22/06/29 20:00"}
         ],
 
+    },
+    onInit(){
+        this.initWs();
     },
     changInput(e){
         this.input = e.value
     },
     send(){
-//        this.ws.send("test")
+        this.isSending = true;
+
+        var time = new Date().toLocaleString('zh-CN');
         this.array.push(
-            {avatar:"/common/icons/user.png", username: "me", msg: this.input,msgCount:"1", date:"22/06/29 20:00"}
-        )
+            {avatar:"/common/icons/user.png", username: "me", msg: this.input,msgCount:"1", date:time}
+        );
+
+        var jsonString = JSON.stringify({
+//            "from": this.user.userId,
+            "text": this.input,
+            "target": this.sendTo
+        })
+
+        if(this.ws!=null) {
+            this.ws.send(jsonString, (err, value) => {
+                if (!err) {
+                    console.log("send success");
+                } else {
+                    console.log("send fail, err:" + JSON.stringify(err));
+                }
+            });
+        }
+        this.isSending = false;
+//        this.$element('chatList').scrollBottom(true);  //滚动到底部，无效
     },
     goUser(){
         console.log("打开用户页面");
@@ -43,26 +76,26 @@ export default {
         this.ws.on('open', (err, value) => {
             console.log("on open, status:" + value.status + ", message:" + value.message);
             // 当收到on('open')事件时，可以通过send()方法与服务器进行通信
-            this.ws.send("Hello, server!", (err, value) => {
-                if (!err) {
-                    console.log("send success");
-                } else {
-                    console.log("send fail, err:" + JSON.stringify(err));
-                }
-            });
+//            this.ws.send("Hello, server!", (err, value) => {
+//                if (!err) {
+//                    console.log("send success");
+//                } else {
+//                    console.log("send fail, err:" + JSON.stringify(err));
+//                }
+//            });
         });
         this.ws.on('message', (err, value) => {
             console.log("on message, message:" + value);
             // 当收到服务器的`bye`消息时（此消息字段仅为示意，具体字段需要与服务器协商），主动断开连接
-            if (value === 'bye') {
-                this.ws.close((err, value) => {
-                    if (!err) {
-                        console.log("close success");
-                    } else {
-                        console.log("close fail, err is " + JSON.stringify(err));
-                    }
-                });
-            }
+//            if (value === 'bye') {
+//                this.ws.close((err, value) => {
+//                    if (!err) {
+//                        console.log("close success");
+//                    } else {
+//                        console.log("close fail, err is " + JSON.stringify(err));
+//                    }
+//                });
+//            }
         });
         this.ws.on('close', (err, value) => {
             console.log("on close, code is " + value.code + ", reason is " + value.reason);
