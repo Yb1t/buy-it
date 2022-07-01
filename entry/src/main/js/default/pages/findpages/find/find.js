421aac6321a87a89dc1b38187d23ccdeea73d826
dnfer <1393178335@qq.com>
2022-07-01 16:48:59 +0800

    1.界面优化 2.增加了videofind页面 3.增加了attentionpage页面 4.增加了mainpages页面 5.修改了页面导航

--- entry/src/main/js/default/pages/findpages/find/find.js
+++ entry/src/main/js/default/pages/findpages/find/find.js
@@ -1,8 +1,16 @@
+import document from '@ohos.document';
 export default {
     data: {
         title: 'World',
         dynamic:[{"image":"/common/icons/boy.png", "content":"111不谢", "imagesrc":"/common/icons/boy.png", "name":"桂电一号"},
                  {"image":"/common/icons/boy.png", "content":"111不谢", "imagesrc":"/common/icons/boy.png", "name":"桂电一号"},
+                 {"image":"/common/icons/boy.png", "content":"111不谢", "imagesrc":"/common/icons/boy.png", "name":"桂电一号"},
+                 {"image":"/common/icons/boy.png", "content":"111不谢", "imagesrc":"/common/icons/boy.png", "name":"桂电一号"},
+
                  ]
+    },
+    tabchange(e){
+        console.info("Tab index: " + e.index )
+
     }
 }
