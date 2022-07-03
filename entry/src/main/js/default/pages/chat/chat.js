36f215a65d6dbb702e98a0c30560b3415ac2946f
hao <106147768@qq.com>
2022-07-03 17:24:09 +0800

    对接webSocket

--- entry/src/main/js/default/pages/chat/chat.js
+++ entry/src/main/js/default/pages/chat/chat.js
@@ -1,12 +1,19 @@
 import router from '@system.router';
 import webSocket from '@ohos.net.webSocket';
-
+import prompt from '@system.prompt';
+import dataRdb from '@ohos.data.rdb'
+const STORE_CONFIG = { name: "RdbTest.db" }
+const SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS EMPLOYEE (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT NOT NULL, AGE INTEGER, SALARY REAL, CODES BLOB)"
+dataRdb.getRdbStore(STORE_CONFIG, 1, function (err, rdbStore) {
+    rdbStore.executeSql(SQL_CREATE_TABLE, null)
+    console.info('create table done.')
+})
 export default {
     data: {
         title: 'World',
         topHeight: "100px",
         ws: null,
-        url: 'ws://haotest.vaiwan.com/websocket/1',
+        url: "ws://haotest.vaiwan.com/ws/",
         user: {
             userId: 1,
             username: "hao",
@@ -22,15 +29,16 @@ export default {
         isSending: false,
         input: "",
         array: [
-            {to: 1, sender: 2, msg:"你好", date: "22/06/29 12:00"},
-            {to: 2, sender: 1, msg:"balbalba", date:"22/06/29 12:00"},
-            {to: 1, sender: 2, msg:"你好你好你好", date:"22/06/29 00:00"},
-            {to: 2, sender: 1, msg:"啦啦啦啦啦", date:"22/06/29 12:00"},
-            {to: 1, sender: 2, msg:"哈哈哈哈哈", date:"22/06/29 20:00"}
+            {receiver: 1, sender: 2, content:"你好", date: "22/06/29 12:00"},
+            {receiver: 2, sender: 1, content:"balbalba", date:"22/06/29 12:00"},
+            {receiver: 1, sender: 2, content:"你好你好你好", date:"22/06/29 00:00"},
+            {receiver: 2, sender: 1, content:"啦啦啦啦啦", date:"22/06/29 12:00"},
+            {receiver: 1, sender: 2, content:"哈哈哈哈哈", date:"22/06/29 20:00"}
         ],
 
     },
     onInit(){
+        this.url = this.url + this.user.userId;
         this.initWs();
     },
     changInput(e){
@@ -41,26 +49,31 @@ export default {
 
         var time = new Date().toLocaleString('zh-CN');
         this.array.push(
-            {avatar:"/common/icons/user.png", username: "me", msg: this.input,msgCount:"1", date:time}
+            {receiver: this.sendTo.userId, sender: this.user.userId, content: this.input, date: time}
         );
 
         var jsonString = JSON.stringify({
-//            "from": this.user.userId,
-            "text": this.input,
-            "target": this.sendTo
+            "type": 1,
+//            "sender": this.user.userId,
+            "target": this.sendTo.userId,
+            "content": this.input
         })
 
         if(this.ws!=null) {
-            this.ws.send(jsonString, (err, value) => {
-                if (!err) {
-                    console.log("send success");
-                } else {
-                    console.log("send fail, err:" + JSON.stringify(err));
-                }
-            });
+            try{
+                this.ws.send(jsonString, (err, value) => {
+                    if (!err) {
+                        console.log("send success");
+                    } else {
+                        console.log("send fail, err:" + JSON.stringify(err));
+                    }
+                });
+            }catch(err){
+                console.log(JSON.stringify(err))
+            }
         }
         this.isSending = false;
-//        this.$element('chatList').scrollBottom(true);  //滚动到底部，无效
+        // TODO this.$element('chatList').scrollBottom(true);  //滚动到底部，无效
     },
     goUser(){
         console.log("打开用户页面");
@@ -75,27 +88,23 @@ export default {
         this.ws = webSocket.createWebSocket();
         this.ws.on('open', (err, value) => {
             console.log("on open, status:" + value.status + ", message:" + value.message);
-            // 当收到on('open')事件时，可以通过send()方法与服务器进行通信
-//            this.ws.send("Hello, server!", (err, value) => {
-//                if (!err) {
-//                    console.log("send success");
-//                } else {
-//                    console.log("send fail, err:" + JSON.stringify(err));
-//                }
-//            });
+            prompt.showToast("成功连接服务器")
         });
         this.ws.on('message', (err, value) => {
             console.log("on message, message:" + value);
-            // 当收到服务器的`bye`消息时（此消息字段仅为示意，具体字段需要与服务器协商），主动断开连接
-//            if (value === 'bye') {
-//                this.ws.close((err, value) => {
-//                    if (!err) {
-//                        console.log("close success");
-//                    } else {
-//                        console.log("close fail, err is " + JSON.stringify(err));
-//                    }
-//                });
-//            }
+            var message = JSON.parse(value);
+            if (message.type === 0){
+                prompt.showToast({
+                    message: '对方已屏蔽您的消息'
+                })
+            }else{
+                this.array.push({
+                    sender: message.sender,
+                    receiver: message.target,
+                    content: message.content,
+                    date: new Date().toLocaleString('zh-CN')
+                })
+            }
         });
         this.ws.on('close', (err, value) => {
             console.log("on close, code is " + value.code + ", reason is " + value.reason);
