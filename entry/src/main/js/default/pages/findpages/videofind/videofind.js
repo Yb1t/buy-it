20e9b08df947e40f42886a97472dbe9d85f1f71d
dnfer <1393178335@qq.com>
2022-07-04 23:03:46 +0800

    1.优化了布局 2.增加了关注和收藏图标和响应事件

--- entry/src/main/js/default/pages/findpages/videofind/videofind.js
+++ entry/src/main/js/default/pages/findpages/videofind/videofind.js
@@ -1,9 +1,29 @@
 export default {
     data: {
         title: 'World',
-        videoData:["/common/video/video2.mp4","/common/video/video1.mp4","/common/video/video0.mp4"]
+        videoData:["/common/video/video2.mp4","/common/video/video1.mp4","/common/video/video0.mp4"],
+        likeSrc:'/common/icons/like1.png',
+        saveSrc:'/common/icons/star1.png'
     },
     onfinish(){
 
+    },
+    likeClick(){
+        if (this.likeSrc==this.$t('/common/icons/like1.png')) {
+            this.likeSrc=this.$t('/common/icons/like2.png')
+        }else{
+            this.likeSrc=this.$t('/common/icons/like1.png')
+        }
+    },
+    saveClick(){
+        if (this.saveSrc==this.$t('/common/icons/star1.png')) {
+            this.saveSrc=this.$t('/common/icons/star2.png')
+        }else{
+            this.saveSrc=this.$t('/common/icons/star1.png')
+        }
+    },
+    shareClick(){
+
     }
+
 }
