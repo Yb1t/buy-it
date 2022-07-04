898288c175df018766caaa641211c09d95fc2e26
dnfer <1393178335@qq.com>
2022-07-04 18:05:04 +0800

    1.优化了布局 2.修改了videofind页面布局

--- entry/src/main/js/default/pages/findpages/videofind/videofind.js
+++ entry/src/main/js/default/pages/findpages/videofind/videofind.js
@@ -1,5 +1,9 @@
 export default {
     data: {
-        title: 'World'
+        title: 'World',
+        videoData:["/common/video/video2.mp4","/common/video/video1.mp4","/common/video/video0.mp4"]
+    },
+    onfinish(){
+
     }
 }
