e06c3abc26ee98847824c4b096d96f86c4382b40
dnfer <1393178335@qq.com>
2022-07-05 12:00:20 +0800

    1.增加了部分图片数据 2.优化结构

--- entry/src/main/js/default/pages/findpages/find/find.js
+++ entry/src/main/js/default/pages/findpages/find/find.js
@@ -2,8 +2,8 @@ import document from '@ohos.document';
 export default {
     data: {
         title: 'World',
-        dynamic:[{"image":"/common/icons/boy.png", "content":"111不谢", "imagesrc":"/common/icons/boy.png", "name":"桂电一号"},
-                 {"image":"/common/icons/boy.png", "content":"111不谢", "imagesrc":"/common/icons/boy.png", "name":"桂电一号"},
+        dynamic:[{"image":"/common/findImage/image11.png", "content":"好吃的", "imagesrc":"/common/icons/boy.png", "name":"桂电美食家"},
+                 {"image":"/common/findImage/image12.png", "content":"超有料的水果桶", "imagesrc":"/common/icons/boy.png", "name":"超能吃大嘴巴"},
                  {"image":"/common/icons/boy.png", "content":"111不谢", "imagesrc":"/common/icons/boy.png", "name":"桂电一号"},
                  {"image":"/common/icons/boy.png", "content":"111不谢", "imagesrc":"/common/icons/boy.png", "name":"桂电一号"},
 
