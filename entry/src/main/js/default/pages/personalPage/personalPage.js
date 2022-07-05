862ded2439ffc3967469f6fb08c7fffa7fbac2b1
HRui666 <2985054390@qq.com>
2022-07-05 16:58:33 +0800

    个人中心完成全部布局（图标内容替换）

--- entry/src/main/js/default/pages/personalPage/personalPage.js
+++ entry/src/main/js/default/pages/personalPage/personalPage.js
@@ -1,5 +1,36 @@
 export default {
     data: {
-        title: 'World'
+        title: 'World',
+        collectionData:[
+            {"collectionDataIcons":"/common/images/icon_star.png","collectionDataName":"收藏"},
+            {"collectionDataIcons":"/common/icons/icon_rss.png","collectionDataName":"订阅店铺"},
+            {"collectionDataIcons":"/common/icons/icon_history.png","collectionDataName":"足迹"},
+            {"collectionDataIcons":"/common/icons/icon_wallet-one.png","collectionDataName":"零钱"}
+        ],
+        orderDate:[
+            {"orderIcons":"/common/icons/icon_bank-card.png","orderName":"待付款"},
+            {"orderIcons":"/common/icons/icon_shopping-bag.png","orderName":"待发货"},
+            {"orderIcons":"/common/images/icon_transporter.png","orderName":"待收货"},
+            {"orderIcons":"/common/icons/icon_comment.png","orderName":"待评价"},
+            {"orderIcons":"/common/icons/icon_expenses.png","orderName":"退款/售后"}
+        ],
+        vipData:[
+            {"vipDataIcons":"/common/icons/icon_light-member.png","vipDataName":"会员中心","vipDataDital":"查看权益"},
+            {"vipDataIcons":"/common/icons/icon_coupon.png","vipDataName":"领券中心","vipDataDital":"天天领5折券"},
+            {"vipDataIcons":"/common/icons/icon_weixin-cards-offers.png","vipDataName":"红包卡券","vipDataDital":"查看可用优惠"},
+            {"vipDataIcons":"/common/icons/icon_red-envelope.png","vipDataName":"红包签到","vipDataDital":"点击领取红包"},
+        ],
+        tryOneData:[
+            {"tryOneDataIcons":"/common/icons/icon_local (2).png","tryOneDataName":"收货地址"},
+            {"tryOneDataIcons":"/common/icons/icon_robot-one.png","tryOneDataName":"官方客服"},
+            {"tryOneDataIcons":"/common/icons/icon_express-delivery.png","tryOneDataName":"我的快递"},
+            {"tryOneDataIcons":"/common/icons/icon_comment-one.png","tryOneDataName":"我的评价"}
+        ],
+        tryTwoData:[
+            {"tryOneDataIcons":"/common/icons/icon_ticket.png","tryOneDataName":"领券中心"},
+            {"tryOneDataIcons":"/common/icons/icon_financing-one.png","tryOneDataName":"来摇现金"},
+            {"tryOneDataIcons":"/common/icons/icon_vip.png","tryOneDataName":"店铺会员"},
+            {"tryOneDataIcons":"/common/icons/icon_system.png","tryOneDataName":"更多"}
+        ]
     }
 }
