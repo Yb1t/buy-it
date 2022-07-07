270a7d1f38dc9dbd6a82b98e08aa08757f092cae
HRui666 <2985054390@qq.com>
2022-07-07 23:03:11 +0800

    完成从后端获取视频并播放

--- entry/src/main/js/default/pages/findpages/videofind/videofind.js
+++ entry/src/main/js/default/pages/findpages/videofind/videofind.js
@@ -1,3 +1,4 @@
+import http from '@ohos.net.http';
 export default {
     data: {
         title: 'World',
@@ -6,6 +7,11 @@ export default {
         mute: 'false',
         message:'',
         content:'',
+        myResult:{
+            "body":[{}],
+            "code":100,
+            "msg":"街火速成功"
+        },
         textData: [{
                        'likeNum': 1222,
                        'saveNum': 14,
@@ -16,30 +22,120 @@ export default {
                        'name': '@跑酷达人',
                        'title': '今日一小伙在街头跑酷',
                        'video': '/common/video/video2.mp4',
-                   }, {
-                       'likeNum': 12,
-                       'saveNum': 14555,
-                       'commentNum': '',
-                       'shareNum': 442,
-                       'likeSrc': '/common/icons/like1.png',
-                       'saveSrc': '/common/icons/star1.png',
-                       'name': '@烟花达人',
-                       'title': '今日一小伙在街头放烟花',
-                       'video': '/common/video/video1.mp4'
-                   }, {
-                       'likeNum': 17777,
-                       'saveNum': 577,
+                   },
+                   {
+                       'likeNum': 1222,
+                       'saveNum': 14,
                        'commentNum': '',
                        'shareNum': 44,
                        'likeSrc': '/common/icons/like1.png',
                        'saveSrc': '/common/icons/star1.png',
-                       'name': '@玩具达人',
-                       'title': '今日一小伙在街头穿玩偶套装',
-                       'video': '/common/video/video0.mp4'
-                   }],
+                       'name': '@跑酷达人',
+                       'title': '今日一小伙在街头跑酷',
+                       'video': '/common/video/video2.mp4',
+                   },
+                   {
+                       'likeNum': 1222,
+                       'saveNum': 14,
+                       'commentNum': '',
+                       'shareNum': 44,
+                       'likeSrc': '/common/icons/like1.png',
+                       'saveSrc': '/common/icons/star1.png',
+                       'name': '@跑酷达人',
+                       'title': '今日一小伙在街头跑酷',
+                       'video': '/common/video/video2.mp4',
+                   },
+                   {
+                       'likeNum': 1222,
+                       'saveNum': 14,
+                       'commentNum': '',
+                       'shareNum': 44,
+                       'likeSrc': '/common/icons/like1.png',
+                       'saveSrc': '/common/icons/star1.png',
+                       'name': '@跑酷达人',
+                       'title': '今日一小伙在街头跑酷',
+                       'video': '/common/video/video2.mp4',
+                   },
+                   {
+                       'likeNum': 1222,
+                       'saveNum': 14,
+                       'commentNum': '',
+                       'shareNum': 44,
+                       'likeSrc': '/common/icons/like1.png',
+                       'saveSrc': '/common/icons/star1.png',
+                       'name': '@跑酷达人',
+                       'title': '今日一小伙在街头跑酷',
+                       'video': '/common/video/video2.mp4',
+                   },
+                   {
+                       'likeNum': 1222,
+                       'saveNum': 14,
+                       'commentNum': '',
+                       'shareNum': 44,
+                       'likeSrc': '/common/icons/like1.png',
+                       'saveSrc': '/common/icons/star1.png',
+                       'name': '@跑酷达人',
+                       'title': '今日一小伙在街头跑酷',
+                       'video': '/common/video/video2.mp4',
+                   },
+                   {
+                       'likeNum': 1222,
+                       'saveNum': 14,
+                       'commentNum': '',
+                       'shareNum': 44,
+                       'likeSrc': '/common/icons/like1.png',
+                       'saveSrc': '/common/icons/star1.png',
+                       'name': '@跑酷达人',
+                       'title': '今日一小伙在街头跑酷',
+                       'video': '/common/video/video2.mp4',
+                   },
+                   {
+                       'likeNum': 1222,
+                       'saveNum': 14,
+                       'commentNum': '',
+                       'shareNum': 44,
+                       'likeSrc': '/common/icons/like1.png',
+                       'saveSrc': '/common/icons/star1.png',
+                       'name': '@跑酷达人',
+                       'title': '今日一小伙在街头跑酷',
+                       'video': '/common/video/video2.mp4',
+                   },
+
+
+        ],
         commentData:[{'name':'李四','comment':'好看'}],
         commentData1:[{'name':'王五','comment':'瞧一瞧'}],
         commentData2:[{'name':'赵七','comment':'路过'}],
+    },
+    onInit() {
+        // 每一个httpRequest对应一个http请求任务，不可复用
+        let httpRequest = http.createHttp();
+        httpRequest.request(
+        // 填写http请求的url地址，可以带参数也可以不带参数。URL地址需要开发者自定义。GET请求的参数可以在extraData中指定
+            "http://huangrui.vaiwan.com/video",
+            {
+
+            }, (err, data) => {
+            if (!err) {
+                // data.result为http响应内容，可根据业务需要进行解析
+                console.info('Result:' + data.result);
+                console.info('code:' + data.responseCode);
+                // data.header为http响应头，可根据业务需要进行解析
+                console.info('header:' + data.header);
+
+                this.myResult = JSON.parse(data.result);
+                this.textData = this.myResult.body;
+
+                console.info("接受到的code数据: "+this.myResult.code)
+                console.info("接受到的msg数据: "+this.myResult.msg)
+                console.info("接受到的order数据: "+this.textData)
+
+
+            } else {
+                console.info('error:' + err.message);
+            }
+        });
+
     },
     onfinish() {
     },
